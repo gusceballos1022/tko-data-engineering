c5d2f1fd788c8cd385d36365192d96e1f0a291f3
Gus Ceballos <82061027+gusceballos1022@users.noreply.github.com>
2023-02-24 14:36:57 -0600

    done

--- steps/05_LastStep_fahrenheit_to_celsius_udf/app.py
+++ steps/05_LastStep_fahrenheit_to_celsius_udf/app.py
@@ -17,6 +17,9 @@ def main(temp_f: float) -> float:
 
 # This is a change to the code 
 
+def main(temp_f: float) -> float:
+    return (float(temp_f) - 32) * (5/9)
+
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
 if __name__ == '__main__':
